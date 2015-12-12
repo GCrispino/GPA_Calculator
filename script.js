a295ad861d0b507561f6b0843611975de4a0f190
GCrispino <gabriel.crispino@hotmail.com>
2015-12-12 05:19:46 -0700

    Add GPA showing

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function calculateGPA() {
+function getGPA() {
 	var classes = document.getElementsByClassName('classes');
 	var total = 0,totalCredits = 0,sumGrades = 0;
 
@@ -19,4 +19,30 @@ function calculateGPA() {
 	if (isNaN(total))
 		total == 0;
 
+	return total;
+}
+
+function calculateGPA(){
+	var GPA = getGPA();
+	var GPA_show = document.createElement('div');
+	var GPA_show_text = document.createElement('p');
+	var container = document.getElementById('container');
+
+	// GPA_show = document.getElementById('GPA_show');
+	// if (GPA_show){
+	// 	GPA_show_text = GPA_show.childNodes[0].innerHTML;
+	// 	GPA_show_text = "Your GPA is " + GPA;
+	// }
+	// else{
+	// 	GPA_show = document.createElement('div');
+	// 	GPA_show_text = document.createElement('p');
+	// }
+
+	GPA_show.id = 'GPA_show';
+	GPA_show_text.innerHTML = "Your GPA is " + GPA;
+	GPA_show.appendChild(GPA_show_text);
+
+	container.appendChild(GPA_show);
+
+	console.log(GPA);
 }
